06442cc96fb08e24046075481b16d3dfb727ec51
Mateo Ortiz <jeusmateo@gmail.com>
2021-09-16 22:07:33 -0500

    Creado un script para el nivel ganado

--- Assets/Scripts/LevelWin.cs
+++ Assets/Scripts/LevelWin.cs
@@ -18,7 +18,14 @@ public class LevelWin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Camera.main.GetComponent<player_camera>().guiMenu.activeInHierarchy &&
+            !Camera.main.GetComponent<player_camera>().guiConfig.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                LoadNextLevel();
+            }
+        }
     }
 
     public void LoadNextLevel()
